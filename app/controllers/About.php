027ea096f423fd98b21c2dd5cfc424e344c8127a
ViqriFebriana <muhammadviqrifebriana@gmail.com>
2021-09-30 16:37:27 +0700

    menyelesaikan vidio view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,10 +1,20 @@
 <?php  
 
-class About {
-   public function index($nama = 'Viqri', $pekerjaan = 'Mahasiswa'){
-      echo "Hallo, Nama saya $nama, saya adalah seorang $pekerjaan";
+class About extends controller{
+   public function index($nama = 'Viqri', $pekerjaan = 'Mahasiswa', $umur = 20){
+
+      $data['nama'] = $nama;
+      $data['pekerjaan'] = $pekerjaan;
+      $data['umur'] = $umur;
+      $data['judul'] = 'About Me';
+      $this->view('templates/header', $data);
+      $this->view('about/index', $data);
+      $this->view('templates/footer');
    }
    public function page(){
-      echo 'About/page';
+      $data['judul'] = 'pages';
+      $this->view('templates/header', $data);
+      $this->view('About/page');
+      $this->view('templates/footer');
    }
 }
